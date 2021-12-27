65f410c23bfeba621f69a067f0f2bc059e91a24b
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-12-27 15:15:46 -0300

    Added method Roteamento to class StartUp

--- Alura.ListaLeitura.App/StartUp.cs
+++ Alura.ListaLeitura.App/StartUp.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
@@ -11,9 +12,27 @@ namespace Alura.ListaLeitura.App
     {
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(LivrosParaLer);
+            app.Run(Roteamento);
         }
 
+        public Task Roteamento(HttpContext context)
+        {
+            var repo = new LivroRepositorioCSV();
+            var caminhosAtendidos = new Dictionary<string, string>
+            {
+                { "/livros/paraler", repo.ParaLer.ToString() },
+                { "/livros/lendo", repo.Lendo.ToString() },
+                { "/livros/lidos", repo.Lidos.ToString() }
+            };
+            
+            if (caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                return context.Response.WriteAsync(caminhosAtendidos[context.Request.Path]);
+            }
+            
+            return context.Response.WriteAsync("Caminho inexistente");
+        }
+        
         public Task LivrosParaLer(HttpContext context)
         {
             var repo = new LivroRepositorioCSV();
