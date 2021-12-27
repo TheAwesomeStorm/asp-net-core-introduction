e3e33fc6299ddc531475dc433dcd5c1469e9cfd8
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-12-27 15:00:11 -0300

    Added instruction for StartUp class methods

--- Alura.ListaLeitura.App/StartUp.cs
+++ Alura.ListaLeitura.App/StartUp.cs
@@ -1,15 +1,23 @@
-﻿namespace Alura.ListaLeitura.App
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Http;
+
+namespace Alura.ListaLeitura.App
 {
     public class StartUp
     {
-        public void Configure()
+        public void Configure(IApplicationBuilder app)
         {
-            
+            app.Run(LivrosParaLer);
         }
 
-        public void LivrosParaLer()
+        public Task LivrosParaLer(HttpContext context)
         {
-            
+            var repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(repo.ParaLer.ToString());
         }
     }
 }
