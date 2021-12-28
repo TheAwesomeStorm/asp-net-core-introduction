0726b5db48e183214139519861b5df98298cb380
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-12-28 13:53:05 -0300

    Added static class RouteDelegates

---
+++ Alura.ListaLeitura.App/RouteDelegates.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Alura.ListaLeitura.App
+{
+    public static class RouteDelegates
+    {
+        private static LivroRepositorioCsv _repositorioCsv;
+
+        static RouteDelegates()
+        {
+            _repositorioCsv = new LivroRepositorioCsv();
+        }
+
+        private static string CarregarArquivoHtml(string fileName)
+        {
+            var filePath = $"HTML/{fileName}.html";
+            using (var arquivo = File.OpenText(filePath))
+            {
+                return arquivo.ReadToEnd();
+            }
+        }
+
+        public static Task ProcessarFormulario(HttpContext context)
+        {
+            var livro = new Livro()
+            {
+                Titulo = context.Request.Form["titulo"].First(),
+                Autor = context.Request.Form["autor"].First(),
+            };
+            _repositorioCsv.Incluir(livro);
+            return context.Response.WriteAsync("Livro adicionado a partir do formulário");
+        }
+
+        public static Task ExibeFormulario(HttpContext context)
+        {
+            var html = CarregarArquivoHtml("formulario");
+            return context.Response.WriteAsync(html);
+        }
+
+        public static Task ExibeDetalhes(HttpContext context)
+        {
+            var id = Convert.ToInt32(context.GetRouteValue("id"));
+            var livro = _repositorioCsv.Todos.First(l => l.Id == id);
+            return context.Response.WriteAsync(livro.Detalhes());
+        }
+
+        public static Task NovoLivroParaLer(HttpContext context)
+        {
+            var livro = new Livro()
+            {
+                Titulo = Convert.ToString(context.GetRouteValue("nome")),
+                Autor = Convert.ToString(context.GetRouteValue("autor"))
+            };
+            _repositorioCsv.Incluir(livro);
+            return context.Response.WriteAsync("Novo livro adicionado com sucesso");
+        }
+
+        public static Task LivrosParaLer(HttpContext context)
+        {
+            var html = CarregarArquivoHtml("para-ler");
+
+            foreach (var livro in _repositorioCsv.ParaLer.Livros)
+            {
+                html = html
+                    .Replace("#NOVO-ITEM#", $"<li>{livro.Titulo} - {livro.Autor}</li>#NOVO-ITEM#");
+            }
+
+            html = html.Replace("#NOVO-ITEM#", "");
+            
+            return context.Response.WriteAsync(html);
+        }
+
+        public static Task LivrosLendo(HttpContext context)
+        {
+            return context.Response.WriteAsync(_repositorioCsv.Lendo.ToString());
+        }
+
+        public static Task LivrosLidos(HttpContext context)
+        {
+            return context.Response.WriteAsync(_repositorioCsv.Lidos.ToString());
+        }
+    }
+}
--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -1,25 +1,12 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Alura.ListaLeitura.App.Negocio;
-using Alura.ListaLeitura.App.Repositorio;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
+using static Alura.ListaLeitura.App.RouteDelegates;
 
 namespace Alura.ListaLeitura.App
 {
     public class Startup
     {
-        private LivroRepositorioCsv _repositorioCsv;
-
-        public Startup()
-        {
-            _repositorioCsv = new LivroRepositorioCsv();
-        }
-        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRouting();
@@ -40,75 +27,6 @@ namespace Alura.ListaLeitura.App
             
             // app.Run(Roteamento);
             app.UseRouter(routes);
-        }
-
-        private Task ProcessarFormulario(HttpContext context)
-        {
-            var livro = new Livro()
-            {
-                Titulo = context.Request.Form["titulo"].First(),
-                Autor = context.Request.Form["autor"].First(),
-            };
-            _repositorioCsv.Incluir(livro);
-            return context.Response.WriteAsync("Livro adicionado a partir do formulário");
-        }
-
-        private Task ExibeFormulario(HttpContext context)
-        {
-            var html = CarregarArquivoHTML("formulario");
-            return context.Response.WriteAsync(html);
-        }
-
-        private string CarregarArquivoHTML(string fileName)
-        {
-            var filePath = $"HTML/{fileName}.html";
-            using (var arquivo = File.OpenText(filePath))
-            {
-                return arquivo.ReadToEnd();
-            }
-        }
-
-        private Task ExibeDetalhes(HttpContext context)
-        {
-            var id = Convert.ToInt32(context.GetRouteValue("id"));
-            var livro = _repositorioCsv.Todos.First(l => l.Id == id);
-            return context.Response.WriteAsync(livro.Detalhes());
-        }
-
-        private Task NovoLivroParaLer(HttpContext context)
-        {
-            var livro = new Livro()
-            {
-                Titulo = Convert.ToString(context.GetRouteValue("nome")),
-                Autor = Convert.ToString(context.GetRouteValue("autor"))
-            };
-            _repositorioCsv.Incluir(livro);
-            return context.Response.WriteAsync("Novo livro adicionado com sucesso");
-        }
-
-        private Task LivrosParaLer(HttpContext context)
-        {
-            var html = CarregarArquivoHTML("para-ler");
-
-            foreach (var livro in _repositorioCsv.ParaLer.Livros)
-            {
-                html = html
-                    .Replace("#NOVO-ITEM#", $"<li>{livro.Titulo} - {livro.Autor}</li>#NOVO-ITEM#");
-            }
-
-            html = html.Replace("#NOVO-ITEM#", "");
-            
-            return context.Response.WriteAsync(html);
-        }
-
-        private Task LivrosLendo(HttpContext context)
-        {
-            return context.Response.WriteAsync(_repositorioCsv.Lendo.ToString());
-        }
-
-        private Task LivrosLidos(HttpContext context)
-        {
-            return context.Response.WriteAsync(_repositorioCsv.Lidos.ToString());
         }
     }
 }
